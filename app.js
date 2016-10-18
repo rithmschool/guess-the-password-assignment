69e886f40617f0de30ddd4a5839fa65abc860afc
Matt Lane <lane.matthew@gmail.com>
2016-10-18 12:16:57 -0700

    finish basic functionality

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
-var wordCount = 12;
+var wordCount = 15;
 var guessCount = 4;
 
 var start = document.getElementById('start');
 start.addEventListener('click', function() {
   var startScreen = document.getElementById('start-screen');
   var gameScreen = document.getElementById('game-screen');
-  startScreen.classList.toggle('hide');
-  startScreen.classList.toggle('show');
-  gameScreen.classList.toggle('show');
-  gameScreen.classList.toggle('hide');
+  toggleClasses(startScreen, 'hide', 'show');
+  toggleClasses(gameScreen, 'hide', 'show');
   startGame();
 });
 
 function startGame() {
   var wordList = document.getElementById("word-list");
-  var randomWords = getRandomValues(words, 12);
+  var guessesRemaining = document.getElementById("guesses-remaining");
+  var winner = document.getElementById("winner");
+  var loser = document.getElementById("loser");
+  var randomWords = getRandomValues(words, wordCount);
   randomWords.forEach(function(word) {
     var li = document.createElement("li");
     li.innerText = word;
     wordList.appendChild(li);
   });
   var password = getRandomValues(randomWords, 1)[0];
+  wordList.addEventListener('click', function(e) {
+    if (e.target.tagName === "LI" && !e.target.classList.contains("disabled")) {
+      var guess = e.target.innerText;
+      var similarityScore = compareWords(guess, password);
+      e.target.classList.add("disabled");
+      e.target.innerText = e.target.innerText + " --> Matching Letters: " + similarityScore;
+      guessCount--;
+      guessesRemaining.innerText = "Guesses remaining: " + guessCount + ".";
+      if (similarityScore === password.length) {
+        toggleClasses(winner, 'hide', 'show');
+      } else if (guessCount === 0) {
+        toggleClasses(loser, 'hide', 'show');
+      }
+    }
+  });
+  guessesRemaining.innerText = "Guesses remaining: " + guessCount + ".";
+}
+
+function toggleClasses(element) {
+  for (var i = 1; i < arguments.length; i++) {
+    element.classList.toggle(arguments[i]);
+  }
+}
+
+function compareWords(word1, word2) {
+  if (word1.length !== word2.length) throw "Words must have the same length";
+  var count = 0;
+  for (var i = 0; i < word1.length; i++) {
+    if (word1[i] === word2[i]) count++;
+  }
+  return count;
 }
 
 function shuffle(array) {
@@ -38,5 +70,5 @@ function shuffle(array) {
 }
 
 function getRandomValues(array, numberOfVals) {
-  return shuffle(array).slice(0, numberOfVals + 1);
+  return shuffle(array).slice(0, numberOfVals);
 }
