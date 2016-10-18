1b3c15563c46f8fdea3da228c33fc46e48fbb357
Matt Lane <lane.matthew@gmail.com>
2016-10-18 13:42:34 -0700

    adding let and const

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var wordCount = 15;
-  var guessCount = 4;
-  var password = '';
+// refactors:
 
-  var start = document.getElementById('start');
+// 1. let
+// 2. const
+// 3. template strings
+// 4. arrow functions
+// 5. rest
+// 6. destructuring assignment
+// bonus: default params?
+
+document.addEventListener('DOMContentLoaded', function() {
+  const wordCount = 15;
+  let guessCount = 4;
+  let password = '';
+
+  let start = document.getElementById('start');
   start.addEventListener('click', function() {
     toggleClasses(document.getElementById('start-screen'), 'hide', 'show');
     toggleClasses(document.getElementById('game-screen'), 'hide', 'show');
@@ -12,10 +22,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function startGame() {
     // get random words and append them to the DOM
-    var wordList = document.getElementById("word-list");
-    var randomWords = getRandomValues(words, wordCount);
+    let wordList = document.getElementById("word-list");
+    let randomWords = getRandomValues(words, wordCount);
     randomWords.forEach(function(word) {
-      var li = document.createElement("li");
+      let li = document.createElement("li");
       li.innerText = word;
       wordList.appendChild(li);
     });
@@ -31,8 +41,8 @@ document.addEventListener('DOMContentLoaded', function() {
   function updateGame(e) {
     if (e.target.tagName === "LI" && !e.target.classList.contains("disabled")) {
       // grab guessed word, check it against password, update view
-      var guess = e.target.innerText;
-      var similarityScore = compareWords(guess, password);
+      let guess = e.target.innerText;
+      let similarityScore = compareWords(guess, password);
       e.target.classList.add("disabled");
       e.target.innerText = e.target.innerText + " --> Matching Letters: " + similarityScore;
       setGuessCount(guessCount - 1);
@@ -54,28 +64,28 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function toggleClasses(element) {
-    for (var i = 1; i < arguments.length; i++) {
+    for (let i = 1; i < arguments.length; i++) {
       element.classList.toggle(arguments[i]);
     }
   }
 
   function compareWords(word1, word2) {
     if (word1.length !== word2.length) throw "Words must have the same length";
-    var count = 0;
-    for (var i = 0; i < word1.length; i++) {
+    let count = 0;
+    for (let i = 0; i < word1.length; i++) {
       if (word1[i] === word2[i]) count++;
     }
     return count;
   }
 
   function shuffle(array) {
-    var arrayCopy = array.slice();
-    for (var idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
+    let arrayCopy = array.slice();
+    for (let idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
       // generate a random index between 0 and idx1 (inclusive)
-      var idx2 = Math.floor(Math.random() * (idx1 + 1));
+      let idx2 = Math.floor(Math.random() * (idx1 + 1));
 
       // swap elements at idx1 and idx2
-      var temp = arrayCopy[idx1];
+      let temp = arrayCopy[idx1];
       arrayCopy[idx1] = arrayCopy[idx2];
       arrayCopy[idx2] = temp;
     }
