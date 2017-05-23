b3c0da2b10344279b2a3670c4107c64270ce557e
Elie Schoppik <eschoppik@gmail.com>
2017-05-23 10:54:30 -0700

    ten guesses and reordering functions for readability

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var wordCount = 15;
+  var wordCount = 10;
   var guessCount = 4;
   var password = '';
 
@@ -9,6 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
     toggleClasses(document.getElementById('game-screen'), 'hide', 'show');
     startGame();
   });
+
+  function toggleClasses(element) {
+    for (var i = 1; i < arguments.length; i++) {
+      element.classList.toggle(arguments[i]);
+    }
+  }
 
   function startGame() {
     // get random words and append them to the DOM
@@ -26,6 +32,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // add update listener for clicking on a word
     wordList.addEventListener('click', updateGame);
+  }
+
+  function getRandomValues(array, numberOfVals) {
+    return shuffle(array).slice(0, numberOfVals);
+  }
+
+  function shuffle(array) {
+    var arrayCopy = array.slice();
+    for (var idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
+      // generate a random index between 0 and idx1 (inclusive)
+      var idx2 = Math.floor(Math.random() * (idx1 + 1));
+
+      // swap elements at idx1 and idx2
+      var temp = arrayCopy[idx1];
+      arrayCopy[idx1] = arrayCopy[idx2];
+      arrayCopy[idx2] = temp;
+    }
+    return arrayCopy;
+  }
+
+  function setGuessCount(newCount) {
+    guessCount = newCount;
+    document.getElementById("guesses-remaining").innerText = "Guesses remaining: " + guessCount + ".";
   }
 
   function updateGame(e) {
@@ -48,17 +77,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-  function setGuessCount(newCount) {
-    guessCount = newCount;
-    document.getElementById("guesses-remaining").innerText = "Guesses remaining: " + guessCount + ".";
-  }
-
-  function toggleClasses(element) {
-    for (var i = 1; i < arguments.length; i++) {
-      element.classList.toggle(arguments[i]);
-    }
-  }
-
   function compareWords(word1, word2) {
     if (word1.length !== word2.length) throw "Words must have the same length";
     var count = 0;
@@ -66,23 +84,5 @@ document.addEventListener('DOMContentLoaded', function() {
       if (word1[i] === word2[i]) count++;
     }
     return count;
-  }
-
-  function shuffle(array) {
-    var arrayCopy = array.slice();
-    for (var idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
-      // generate a random index between 0 and idx1 (inclusive)
-      var idx2 = Math.floor(Math.random() * (idx1 + 1));
-
-      // swap elements at idx1 and idx2
-      var temp = arrayCopy[idx1];
-      arrayCopy[idx1] = arrayCopy[idx2];
-      arrayCopy[idx2] = temp;
-    }
-    return arrayCopy;
-  }
-
-  function getRandomValues(array, numberOfVals) {
-    return shuffle(array).slice(0, numberOfVals);
   }
 });
